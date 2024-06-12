962a3dc3dbe9807c84db3bbcf6e858644b910b24
Xiahaifeng <xiahaifeng2000@gmail.com>
2024-06-12 18:24:28 +0800

    solution2719

--- feng-core/src/main/java/com/LoveSea/fengCore/study/leetCode/Solution2719.java
+++ feng-core/src/main/java/com/LoveSea/fengCore/study/leetCode/Solution2719.java
@@ -10,19 +10,40 @@ package com.LoveSea.fengCore.study.leetCode;
  */
 
 public class Solution2719 {
+    int mod = 1000000007;
     private int min_sum;
     private int max_sum;
-    public int count(String num1, String num2, int min_sum, int max_sum) {
+    private String num;
 
+    public int count(String num1, String num2, int min_sum, int max_sum) {
+        this.min_sum = min_sum;
+        this.max_sum = max_sum;
+        this.num = num2;
+        int res = dfs(num.length() - 1, 0, true);
+        long num1Long = Long.parseLong(num1);
+        num1Long--;
+        this.num = String.valueOf(num1Long);
+        res -= dfs(num.length() - 1, 0, true);
+        return (res + mod) % mod;
     }
 
-    private int dfs(int pos,int sum,boolean limit){
-        if(sum>max_sum){
+    private int dfs(int pos, int sum, boolean limit) {
+        if (sum > max_sum) {
             return 0;
         }
-        if(-1==pos){
-            return sum>=min_sum?1:0;
+        if (-1 == pos) {
+            return sum >= min_sum ? 1 : 0;
         }
+        int up = limit ? num.charAt(pos) - '0' : 9;
+        int res = 0;
+        for (int i = 0; i <= up; i++) {
+            res += (dfs(pos - 1, sum + i, limit && i == up) % mod);
+        }
+        return res;
+    }
 
+    public static void main(String[] args) {
+        Solution2719 solution2719 = new Solution2719();
+        System.out.println(solution2719.count("1", "12", 1, 8));
     }
 }
