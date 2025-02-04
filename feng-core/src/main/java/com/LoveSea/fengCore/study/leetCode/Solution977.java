98f66dc43dfb5cdc319dbce8ee3cfe7c6bc307ec
xiahaifeng <xiahaifeng2000@gmail.com>
2025-02-04 17:22:46 +0800

    Add Solution977: Implement brute force approach for sortedSquares method

--- feng-core/src/main/java/com/LoveSea/fengCore/study/leetCode/Solution977.java
+++ feng-core/src/main/java/com/LoveSea/fengCore/study/leetCode/Solution977.java
@@ -46,6 +46,7 @@ public class Solution977 {
     }
 
     public int[] sortedSquares1(int[] nums) {
+        // 暴力解法
         for (int i = 0; i < nums.length; i++) {
             nums[i] *= nums[i];
         }
