7e819092cfb5ea56e4b41e8fcbf0e1ca78a2c4fd
xiahaifeng <xiahaifeng2000@gmail.com>
2025-02-06 16:43:42 +0800

    Add UrlUtils: Implement downloadUrlTransfer method for URL encoding

---
+++ feng-core/src/main/java/com/LoveSea/fengCore/commons/utils/UrlUtils.java
@@ -0,0 +1,41 @@
+package com.LoveSea.fengCore.commons.utils;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author xiahaifeng
+ */
+
+public class UrlUtils {
+
+    /**
+     * downloadUrlTransfer : 下载地址转码<br>
+     *
+     * @param originalUrl 原始下载地址
+     * @return 转码后的下载地址
+     */
+    public static String downloadUrlTransfer(String originalUrl) {
+        int qMask = originalUrl.indexOf("?");
+        String baseUrl;
+        String params;
+        if (qMask == -1) {
+            baseUrl = originalUrl;
+            params = "";
+        } else {
+            baseUrl = originalUrl.substring(0, qMask);
+            params = originalUrl.substring(qMask);
+        }
+        int lastSlashes = baseUrl.lastIndexOf("/");
+        // 获取域名 例如：http://spr45rkqa.hd-bkt.clouddn.com/xxx/
+        String domain = baseUrl.substring(0, lastSlashes + 1);
+        // 获取文件名
+        String fileName = baseUrl.substring(lastSlashes + 1);
+        // 将文件名进行转码
+        fileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8);
+        // 空格会被转码为+，需要将+转换为%20
+        fileName = fileName.replaceAll("\\+", "%20");
+        return domain + fileName + params;
+    }
+
+}
