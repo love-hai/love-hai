b0faa180bac6d4488332a6ef396c6109e468af93
xiahaifeng <xiahaifeng2000@gmail.com>
2025-02-22 07:41:43 +0800

    2209

---
+++ feng-core/src/main/java/com/LoveSea/fengCore/study/leetCode/Solution2209.java
@@ -0,0 +1,32 @@
+package com.LoveSea.fengCore.study.leetCode;
+
+/**
+ * @author xiahaifeng
+ */
+
+public class Solution2209 {
+    static final int INF = 0x3f3f3f3f;
+
+    public int minimumWhiteTiles(String floor, int numCarpets, int carpetLen) {
+        int n = floor.length();
+        int[][] d = new int[n + 1][numCarpets + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= numCarpets; j++) {
+                d[i][j] = INF;
+            }
+        }
+        for (int j = 0; j <= numCarpets; j++) {
+            d[0][j] = 0;
+        }
+        for (int i = 1; i <= n; i++) {
+            d[i][0] = d[i - 1][0] + (floor.charAt(i - 1) == '1' ? 1 : 0);
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= numCarpets; j++) {
+                d[i][j] = d[i - 1][j] + (floor.charAt(i - 1) == '1' ? 1 : 0);
+                d[i][j] = Math.min(d[i][j], d[Math.max(0, i - carpetLen)][j - 1]);
+            }
+        }
+        return d[n][numCarpets];
+    }
+}
