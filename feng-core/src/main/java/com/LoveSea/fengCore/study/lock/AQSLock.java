cb2a96d32dd1064a5a192994dade5da8fabd4b7e
xiahaifeng <xiahaifeng2000@gmail.com>
2025-03-03 14:08:37 +0800

    Aqs 抽象队列同步锁

--- feng-core/src/main/java/com/LoveSea/fengCore/study/lock/AQSLock.java
+++ feng-core/src/main/java/com/LoveSea/fengCore/study/lock/AQSLock.java
@@ -14,15 +14,16 @@ public class AQSLock {
     Thread owner = null;
     AtomicReference<Node> head = new AtomicReference<>(new Node());
     AtomicReference<Node> tail = new AtomicReference<>(head.get());
+
     public void lock() {
-        if(this.owner == Thread.currentThread()) {
+        if (this.owner == Thread.currentThread()) {
             return;
         }
         Node current = new Node();
         current.thread = Thread.currentThread();
         while (true) {
             Node tailNode = tail.get();
-            if(tail.compareAndSet(tailNode, current)) {
+            if (tail.compareAndSet(tailNode, current)) {
                 tailNode.next = current;
                 current.prev = tailNode;
                 break;
@@ -34,31 +35,30 @@ public class AQSLock {
                 head.set(current);
                 current.prev.next = null;
                 current.prev = null;
+                System.out.println(owner.getName() + "获得锁");
                 return;
             }
             LockSupport.park();
         }
     }
 
-
     public void unlock() {
         if (owner != Thread.currentThread()) {
-            throw new IllegalArgumentException("当前线程不是持有锁的线程");
+            System.out.println(owner.getName() + "是锁的拥有者");
+            throw new IllegalArgumentException(Thread.currentThread().getName() + "不是锁的拥有者");
         }
         Node headNode = head.get();
         Node next = headNode.next;
+        this.owner = null;
         flag.set(false);
-        if(next != null) {
+        if (next != null) {
             LockSupport.unpark(next.thread);
         }
-        this.owner = null;
     }
-
 
     static class Node {
         Node prev;
         Node next;
-
         Thread thread;
     }
 }
--- feng-core/src/main/java/com/LoveSea/fengCore/study/lock/AQSLockExample.java
+++ feng-core/src/main/java/com/LoveSea/fengCore/study/lock/AQSLockExample.java
@@ -1,13 +1,35 @@
 package com.LoveSea.fengCore.study.lock;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * 简单阐述一下AQS的思路
  * 获取锁，获取成功将state设置为1，设置线程
  * 获取失败，检查当前使用的线程是否是请求线程，如果是，state+1。
  * 如果当前使用锁的线程也不是请求线程，那么将请求线程加入到等待队列中，等待唤醒。
  * 这个类模拟 线程加入等待队列。
+ *
  * @author xiahaifeng
  */
 
 public class AQSLockExample {
+    public static void main(String[] args) {
+        AQSLock aqsLock = new AQSLock();
+        AtomicInteger flag = new AtomicInteger();
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 10; j++) {
+                    aqsLock.lock();
+                    try {
+                        Thread.sleep((long) (Math.random()*1000));
+                        System.out.println(flag.getAndIncrement());
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    } finally {
+                        aqsLock.unlock();
+                    }
+                }
+            }).start();
+        }
+    }
 }
