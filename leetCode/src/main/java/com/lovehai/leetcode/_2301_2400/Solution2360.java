d86f7fd6d7edc498680fb567c6257066daacc455
xiahaifeng <xiahaifeng2000@gmail.com>
2025-04-01 11:52:46 +0800

    leetCode

---
+++ leetCode/src/main/java/com/lovehai/leetcode/_2301_2400/Solution2360.java
@@ -0,0 +1,17 @@
+package com.lovehai.leetcode._2301_2400;
+
+/**
+ * 给你一个 n 个节点的 有向图 ，节点编号为 0 到 n - 1 ，其中每个节点 至多 有一条出边。
+ * 图用一个大小为 n 下标从 0 开始的数组 edges 表示，节点 i 到节点 edges[i] 之间有一条有向边。如果节点 i 没有出边，那么 edges[i] == -1 。
+ * 请你返回图中的 最长 环，如果没有任何环，请返回 -1 。
+ * 一个环指的是起点和终点是 同一个 节点的路径。
+ * @author xiahaifeng
+ */
+
+public class Solution2360 {
+    public int longestCycle(int[] edges) {
+        int longestCycle = -1;
+
+        return longestCycle;
+    }
+}
