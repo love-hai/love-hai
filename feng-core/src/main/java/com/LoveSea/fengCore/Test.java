94ff43978b2790f79009c00a4845922c3f990dcb
xiahaifeng <xiahaifeng2000@gmail.com>
2025-04-15 16:00:23 +0800

    remove unused imports and simplify main method in Test class

--- feng-core/src/main/java/com/LoveSea/fengCore/Test.java
+++ feng-core/src/main/java/com/LoveSea/fengCore/Test.java
@@ -1,20 +1,12 @@
 package com.LoveSea.fengCore;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author xiahaifeng
  */
 @Slf4j
 public class Test {
-    public static void main(String[] args) throws Exception {
-        String input = "2193357153266849796-2679987036--";
-        String[] parts = input.split("(?<=-)|(?=-)");
-        // 使用 "-" 分割字符串
+    public static void main(String[] args) {
     }
 }
