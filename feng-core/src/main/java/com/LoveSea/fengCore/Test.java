799b053fc079c898a5046af8ba797cfa051d8ca0
夏海锋 <xiahaifeng2000@gmail.com>
2025-09-15 11:41:08 +0800

    test修改

--- feng-core/src/main/java/com/LoveSea/fengCore/Test.java
+++ feng-core/src/main/java/com/LoveSea/fengCore/Test.java
@@ -7,28 +7,11 @@ package com.LoveSea.fengCore;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Comparator;
-import java.util.Date;
-
 /**
  * @author xiahaifeng
  */
 @Slf4j
 public class Test {
     public static void main(String[] args) {
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.HOUR_OF_DAY, 0);
-        cal.set(Calendar.MINUTE, 0);
-        cal.set(Calendar.SECOND, 0);
-        cal.set(Calendar.MILLISECOND, 0);
-
-        Date today = cal.getTime();
-        log.info("today: {}", today);
     }
 }
